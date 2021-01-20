e7db2390f30e334bd74caeb5503f49e4575efbe7
Andy Dunstall <andydunstall@hotmail.co.uk>
2021-01-20 12:33:43 -0500

    Remove frontend

--- main.py
+++ main.py
@@ -5,16 +5,12 @@ import asyncio
 import logging
 
 from model.model import Model
-from server.server import Server
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="TD-Gammon model.")
     parser.add_argument(
         "--test", action='store_true', help="test model"
-    )
-    parser.add_argument(
-        "--serve", action='store_true', help="run websocket server"
     )
     parser.add_argument(
         "--debug", action='store_true', help="debug logging"
@@ -29,9 +25,7 @@ def main(args):
     logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
 
     m = Model(args.restore)
-    if args.serve:
-        s = Server(m).listen()
-    elif args.test:
+    if args.test:
         m.test()
     else:
         m.train()
